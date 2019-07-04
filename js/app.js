6e0aa7c528784111478755f2618d790136cd9226
Lyndon Panton <LyndonPanton@gmail.com>
2019-07-04 19:45:15 +0100

    Added class for making a person

--- js/app.js
+++ js/app.js
@@ -3,6 +3,34 @@
 window.onload = function(event) {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
+	function Person(full) {
+		name = full;
+
+		this.getFirstName = function() {
+			return name.split(" ")[0];
+		}
+
+		this.getFullName = function() {
+			return name;
+		}
+
+		this.getLastName = function() {
+			return name.split(" ")[1];
+		}
+
+		this.setFirstName = function(newFirst) {
+			name = newFirst + name.split(" ")[1];
+		}
+
+		this.setFullName = function(newFull) {
+			name = newFull;
+		}
+
+		this.setLastName = function(newLast) {
+			name = name.split(" ")[0] + newLast;
+		}
+	}
+
 	function toggle(chevron) {
 		let task = document.getElementById("task");
 
@@ -36,4 +64,37 @@ window.onload = function(event) {
 	form.addEventListener("submit", function(event) {
 		event.preventDefault();
 	});
+
+	// freecodecamp version
+	// var Person = function(firstAndLast) {
+	// 	// Complete the method below and implement the others similarly
+	// 	let name = firstAndLast;
+
+	// 	this.getFirstName = function() {
+	//     	return name.split(" ")[0];
+	// 	};
+
+	// 	this.getFullName = function() {
+	//     	return name;
+	// 	};
+
+	// 	this.getLastName = function() {
+	//     	return name.split(" ")[1];
+	// 	};
+
+	// 	this.setFirstName = function(first) {
+	//     	name = first + " " + name.split(" ")[1];
+	// 	};
+
+	// 	this.setFullName = function(firstAndLast) {
+	//     	name = firstAndLast;
+	// 	};
+
+	// 	this.setLastName = function(last) {
+	//     	name = name.split(" ")[0] + " " + last;
+	// 	};
+	// };
+
+	// var bob = new Person('Bob Ross');
+	// console.log(bob.getFullName());
 };
